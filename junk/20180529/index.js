e4e50c16dd269891f8ee6fb8c48650a61c3740f9
uupaa <uupaa.js@gmail.com>
2018-05-29 14:12:54 +0900

    Update index.js

--- junk/20180529/index.js
+++ junk/20180529/index.js
@@ -1,17 +1,1 @@
-import { VideoController } from "https://uupaa.github.io/Tools/junk/20180529/VideoController.js";
-import { save, load } from "https://uupaa.github.io/Tools/junk/20180529/UserSelectSerializer.js";
-
-[...document.body.querySelectorAll("video")].forEach(v => {
-  if (!v.effected) {
-    v.insertAdjacentHTML("afterend",
-      `<div style="font-size: 300%" onclick="hack(this, this.previousElementSibling)">🎃</div>`);
-  }
-});
-
-window.hack = (jack, video) => {
-  jack.remove();
-  video.insertAdjacentHTML("afterend", `<video-controller></video-controller>`);
-  load();
-  save();
-};
-
+import "https://uupaa.github.io/Tools/junk/20180529/ver1.js";
