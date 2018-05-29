2e5eb847c63173ea97151af95d564c7b10ad2923
uupaa <uupaa.js@gmail.com>
2018-05-29 14:30:10 +0900

    update

--- junk/20180529/index.js
+++ junk/20180529/index.js
@@ -1,2 +1,21 @@
-import(`https://uupaa.github.io/Tools/junk/20180529/ver1.js?t=${Date.now()}`);
+import { VideoController } from "https://uupaa.github.io/Tools/junk/20180529/VideoController.js";
+import { save, load } from "https://uupaa.github.io/Tools/junk/20180529/UserSelectSerializer.js";
+
+//import(`https://uupaa.github.io/Tools/junk/20180529/ver1.js?t=${Date.now()}`);
+
+[...document.body.querySelectorAll("video")].forEach(v => {
+  if (!v.getAttribute("hacked")) {
+    v.insertAdjacentHTML("afterend",
+      `<div style="font-size: 300%" onclick="hack(this, this.previousElementSibling)">🎃</div>`);
+  } else {
+    console.log("hacked");
+  }
+});
+
+window.hack = (jack, video) => {
+  jack.remove();
+  video.insertAdjacentHTML("afterend", `<video-controller></video-controller>`);
+  load();
+  save();
+};
 
--- junk/20180529/ver1.js
+++ junk/20180529/ver1.js
@@ -1,19 +1,1 @@
-import { VideoController } from "https://uupaa.github.io/Tools/junk/20180529/VideoController.js";
-import { save, load } from "https://uupaa.github.io/Tools/junk/20180529/UserSelectSerializer.js";
-
-[...document.body.querySelectorAll("video")].forEach(v => {
-  if (!v.getAttribute("hacked")) {
-    v.insertAdjacentHTML("afterend",
-      `<div style="font-size: 300%" onclick="hack(this, this.previousElementSibling)">🎃</div>`);
-  } else {
-    console.log("hacked");
-  }
-});
-
-window.hack = (jack, video) => {
-  jack.remove();
-  video.insertAdjacentHTML("afterend", `<video-controller></video-controller>`);
-  load();
-  save();
-};
 
