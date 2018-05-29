f74d5ab180c20be6a1dda3edde9b84dded6388df
uupaa <uupaa.js@gmail.com>
2018-05-29 14:09:35 +0900

    Update UserSelectSerializer.js

--- junk/20180529/UserSelectSerializer.js
+++ junk/20180529/UserSelectSerializer.js
@@ -1,9 +1,35 @@
 
 export function save() { // auto save
-  saveSelectIndex("#queries_company_id", "queries_company_id");
+/*
+  saveText("#queries_id_from", "queries_id_from");
+  saveText("#queries_id_to", "queries_id_to");
+  saveSelectbox("#queries_web_status", "queries_web_status");
+  saveSelectbox("#queries_office_id", "queries_office_id");
+  saveSelectbox("#queries_company_id", "queries_company_id");
+  saveSelectbox("#queries_event_id", "queries_event_id");
+  saveSelectbox("#queries_scene_risk_id", "queries_scene_risk_id");
+  saveRadio("#order_order_asc", "order_order_asc");
+  saveRadio("#order_order_desc", "order_order_desc");
+ */
 }
 
-function saveSelectIndex(selector, id) {
+function saveText(selector, id) {
+  const node = document.querySelector(selector);
+  if (node) {
+    node.onchange = () => {
+      localStorage[`junk_20180529$_${id}`] = node.value;
+    };
+  }
+}
+function saveSelectbox(selector, id) {
+  const node = document.querySelector(selector);
+  if (node) {
+    node.onchange = () => {
+      localStorage[`junk_20180529$_${id}`] = node.selectedIndex;
+    };
+  }
+}
+function saveRadio(selector, id) {
   const node = document.querySelector(selector);
   if (node) {
     node.onchange = () => {
@@ -13,10 +39,12 @@ function saveSelectIndex(selector, id) {
 }
 
 export function load() { // auto load
-  loadSelectIndex("#queries_company_id", "queries_company_id");
+/*
+  loadSelectbox("#queries_company_id", "queries_company_id");
+ */
 }
 
-function loadSelectIndex(selector, id) {
+function loadSelectbox(selector, id) {
   const node = document.querySelector(selector);
   if (node) {
     node.selectedIndex = localStorage[`junk_20180529$_${id}`];
