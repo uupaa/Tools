ab0008306332a86180210810d45105009c16c0ae
uupaa <uupaa.js@gmail.com>
2018-05-29 12:36:48 +0900

    Update index.js

--- junk/20180529/index.js
+++ junk/20180529/index.js
@@ -1,5 +1,5 @@
 import { VideoController } from "https://uupaa.github.io/Tools/junk/20180529/VideoController.js";
 
 [...document.body.querySelectorAll("video")].forEach(v => {
-  v.insertAdjacentHTML("afterend", `<video-controller></video-controller>`);
+  v.insertAdjacentHTML("beforebegin", `<video-controller></video-controller>`);
 });
