4514a091d54066455a5a7542298ef359dea08e40
uupaa <uupaa.js@gmail.com>
2018-05-29 13:22:18 +0900

    Update index.js

--- junk/20180529/index.js
+++ junk/20180529/index.js
@@ -1,6 +1,19 @@
 import { VideoController } from "https://uupaa.github.io/Tools/junk/20180529/VideoController.js";
 //import { VideoController } from "./VideoController.js";
 
+/*
 [...document.body.querySelectorAll("video")].forEach(v => {
   v.insertAdjacentHTML("afterend", `<video-controller></video-controller>`);
 });
+ */
+
+[...document.body.querySelectorAll("video")].forEach(v => {
+  v.insertAdjacentHTML("afterend", `<div onclick="hack(this, this.previousElementSibling)">🎃</div>`);
+});
+
+window.hack = (jack, video) => {
+  jack.remove();
+  jack.parentNode.insertAdjacentHTML("afterend", `<video-controller></video-controller>`);
+};
+
+
