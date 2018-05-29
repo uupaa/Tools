c4d34b17ebd7c1979263e0ac40d53eef417897c2
uupaa <uupaa.js@gmail.com>
2018-05-29 12:46:53 +0900

    Update bookmarklet.js

--- junk/20180529/bookmarklet.js
+++ junk/20180529/bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:((s,p)=>{s.src="//uupaa.github.io/Tools/junk/20180529/index.js";p.append(s)})(document.createElement("script"),document.documentElement)
+javascript:((s,p)=>{s.src="//uupaa.github.io/Tools/junk/20180529/index.js";s.type="module";p.append(s)})(document.createElement("script"),document.documentElement)
