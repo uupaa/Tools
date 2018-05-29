13eadadecc623d1c1983382bb256af3046faff11
uupaa <uupaa.js@gmail.com>
2018-05-29 14:12:07 +0900

    Create lib.js

---
+++ junk/20180529/lib.js
@@ -0,0 +1,17 @@
+import { VideoController } from "https://uupaa.github.io/Tools/junk/20180529/VideoController.js";
+import { save, load } from "https://uupaa.github.io/Tools/junk/20180529/UserSelectSerializer.js";
+
+[...document.body.querySelectorAll("video")].forEach(v => {
+  if (!v.effected) {
+    v.insertAdjacentHTML("afterend",
+      `<div style="font-size: 300%" onclick="hack(this, this.previousElementSibling)">🎃</div>`);
+  }
+});
+
+window.hack = (jack, video) => {
+  jack.remove();
+  video.insertAdjacentHTML("afterend", `<video-controller></video-controller>`);
+  load();
+  save();
+};
+
