7cd91d28b3a85921b8245b887415cca1f8430bcf
uupaa <uupaa.js@gmail.com>
2018-05-29 09:15:31 +0900

    Update index.js

--- junk/20180529/index.js
+++ junk/20180529/index.js
@@ -1,1 +1,5 @@
+import { hack } from "https://uupaa.github.io/Tools/junk/20180529/hack.js";
 
+[...document.body.querySelectorAll("video")].forEach(v => {
+  v.insertAdjacentHTML("afterend", `<div style="font-size:x-large" onclick="hack(this, this.previousElementSibling)">🎃</div>`);
+});
